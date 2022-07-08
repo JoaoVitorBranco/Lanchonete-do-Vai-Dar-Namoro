33514148b7d20a4fc318de0e7d86e16a0930c830
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-08 19:56:16 -0300

    realizando a main

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import 'IComida/Batata/Batata.dart';
 import 'IComida/Hambuguer/Hamburguer.dart';
 import 'IComida/IComida.dart';
 import 'IComida/MilkShake/MilkShake.dart';
@@ -6,15 +7,30 @@ import 'enum/paladar.dart';
 import 'enum/pontoDaCarne.dart';
 
 void main() {
-  IComida xburguer = Hamburguer(
-      "Coca-Cola", PontoDaCarne.MALPASSADO, "XBurguer", Paladar.SALGADO, 20, 1);
+  //Cardápio
+  IComida xBurguer = Hamburguer(
+      "Coca-Cola", PontoDaCarne.MALPASSADO, "Cheesas", Paladar.SALGADO, 45, 1);
+  IComida xBacon = Hamburguer(
+      "Suco de Manga", PontoDaCarne.AOPONTO, "Beycas", Paladar.SALGADO, 50, 1);
+  IComida xVegan = Hamburguer("Suco de Banana", PontoDaCarne.BEMPASSADO,
+      "Vegas", Paladar.SALGADO, 65, 1);
 
-  Pessoa casimiro = Pessoa("Casimiro Miguel", 28, [xburguer]);
-  Pessoa annaBeatriz = Pessoa("Anna Beatriz", 28, []);
+  IComida mFrutasVermelhas =
+      MilkShake("Frutas Vermelhas", true, "Frutas", Paladar.DOCE, 25, 1);
+  IComida mChocolate =
+      MilkShake("Chocolate", false, "Chocolas", Paladar.DOCE, 25, 1);
+  IComida mChocomaluco =
+      MilkShake("Nutella", false, "Chocomalucas", Paladar.DOCE, 30, 1);
 
-  Hamburguer xbacon = Hamburguer("Suco de Manga", PontoDaCarne.BEMPASSADO,
-      "Bacon", Paladar.SALGADO, 20, 1);
+  IComida batataFrita =
+      Batata("Ketchup", false, "Batatas", Paladar.SALGADO, 12, 1);
+  IComida batataSupreme =
+      Batata("Ketchup e Cheddar", true, "Batatas", Paladar.SALGADO, 24, 2);
+  IComida batataPicante = Batata(
+      "Ketchup Malagueta, Barbecue", true, "Batatas", Paladar.SALGADO, 48, 4);
 
-  MilkShake murango =
-      MilkShake("Morango", true, "Murango", Paladar.DOCE, 25, 1);
+  Pessoa casimiro = Pessoa("Casimiro Miguel", 28, []);
+  Pessoa anna = Pessoa("Anna Beatriz", 28, []);
+
+  print(casimiro);
 }
